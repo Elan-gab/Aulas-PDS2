38045710e976010ae7a46a965f7714b843aeb19d
Elan <gabrielelansoares@gmail.com>
2024-03-11 16:52:24 -0300

    compilacao e geracao do executavel final desafio (aula 2)

--- aula 2/Desafio/libmatematica.c
+++ aula 2/Desafio/libmatematica.c
@@ -1,4 +1,5 @@
 #include<math.h>
+#include"libmatematica.h"
 
 float calcular_seno(float angulo_radianos) {
 
--- aula 2/Desafio/libmatematica.h
+++ aula 2/Desafio/libmatematica.h
@@ -1,8 +1,6 @@
 #ifndef LIBMATEMATICA_H
 #define LIBMATEMATICA_H
 
-#include "libmatematica.c"
-
 float calcular_seno(float angulo_radianos);
 float calcular_cosseno(float angulo_radianos);
 float calcular_tangente(float angulo_radianos);
