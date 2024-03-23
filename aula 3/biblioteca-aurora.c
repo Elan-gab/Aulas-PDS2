9d879667cccc3753222eef4da026f19e9747453f
Elan <gabrielelansoares@gmail.com>
2024-03-23 16:21:49 -0300

    resolucao biblioteca aurora

---
+++ aula 3/biblioteca-aurora.c
@@ -0,0 +1,62 @@
+#include<malloc.h>
+
+// ================== estruturas ==================
+
+typedef struct livro
+{
+    int id;               // identificador do livro
+    char* titulo;         // titulo do livro
+    char* autor;          // autor do livro
+    char* genero;         // genero do livro
+    int disponibilidade;  // quantidade de copias disponiveis
+}Livro;
+
+typedef struct reserva
+{
+    int userID;  // identificador do usuario que requisitou a reserva
+    Livro l;     // livro a ser reservado
+    int timeout; // tempo limite de reserva de um livro
+}Reserva;
+
+typedef struct evento
+{   
+    int n;              // numero de participantes
+    int* participantes; // vetor dinamico de id dos usuarios participantes
+    int data;           // data do evento
+    char* tema;         // tema do evento
+}Evento;
+
+// ============== implementacao das funcoes ==============
+
+Livro* pesquisar_livro(int id) {
+
+    Livro* p;
+
+    //algoritmo de busca por ID
+
+    return p;
+}
+
+Reserva* reservar_livro(Livro* l, int q, int t, int userID) {
+
+    l->disponibilidade -= q;
+    Reserva* r;
+    r->timeout = t;
+    r->l = *l;
+    r->userID = userID;
+
+    return r;
+}
+
+Reserva* renovar_emprestimo(Reserva* r, int t) {
+
+    r->timeout = t;
+    return r;
+}
+
+void inscricao(Evento* evento, int userID) {
+
+    evento->n++;
+    evento->participantes = (int *)realloc(evento->participantes, evento->n);
+    evento->participantes[evento->n - 1];
+}
---
+++ aula 3/biblioteca-aurora.h
@@ -0,0 +1,35 @@
+#ifndef REQUISITOS_H
+#define REQUISITOS_H
+
+#include "requisitos.c" // arquivo de implementacao. contem o struct Livro
+
+/**
+ * @param id o numero identificador do livro
+ * @return retorna um ponteiro para o livro pesquisado
+*/
+Livro* pesquisar_livro(int id);
+
+/**
+ * @param l ponteiro para o livro a ser reservado
+ * @param q quantidade de reservas
+ * @param t duracao da reserva
+ * @param userID o identificador do requisitante
+ * @return retorna a requisicao de reserva do livro
+*/
+Reserva* reservar_livro(Livro* l, int q, int t, int userID);
+
+
+/**
+ * @param r ponteiro para a reserva a ser renovada
+ * @param t duracao da nova reserva
+ * @return um ponteiro para a nova reserva 
+*/
+Reserva* renovar_emprestimo(Reserva* r, int t);
+
+/**
+ * @param evento ponteiro para o evento no qual o usuario deseja se inscrever
+ * @param userID identificador do usuario
+*/
+void inscricao(Evento* evento, int userID);
+
+#endif
